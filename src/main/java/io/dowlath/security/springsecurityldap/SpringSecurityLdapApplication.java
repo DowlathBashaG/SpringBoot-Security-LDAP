6dc97fff6a8fb51281504d492bb74e2a594f634a
dowlath <dowlathbasha@gmail.com>
2020-07-09 07:34:54 +0530

    initial commit

---
+++ src/main/java/io/dowlath/security/springsecurityldap/SpringSecurityLdapApplication.java
@@ -0,0 +1,12 @@
+package io.dowlath.security.springsecurityldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLdapApplication.class, args);
+	}
+}
